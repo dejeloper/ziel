529e285f331688c3b109188115d97181bc322514
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-07-01 18:11:37 -0500

    Add page redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import { ModeToggle } from "./components/ModeToggle";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <ModeToggle />;
+  return redirect("/dashboard/home");
 }
