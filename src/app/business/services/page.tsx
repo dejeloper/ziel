09b49a6fd6b3fa126bd62fbce4e07cefe94bb9ce
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-07-01 18:09:52 -0500

    Add pricing, services, about, contact, and home pages. All demo pages

---
+++ src/app/business/services/page.tsx
@@ -0,0 +1,9 @@
+export default function ServicesPage() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-slate-800 dark:text-slate-200">
+        Services Page
+      </h1>
+    </>
+  );
+}
---
+++ src/app/dashboard/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-slate-800 dark:text-slate-200">
+        Home Page
+      </h1>
+    </>
+  );
+}
