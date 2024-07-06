b82ff7148b627b7594dd849898c2275b56db4016
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-07-05 21:55:33 -0500

    Update home page and add button variants

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,48 +1,56 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function HomePage() {
-  const title = "Bienvenido a Ziel";
+  const title = "Hola 👋 Ziel ";
   const description =
-    "Ziel es una aplicación de seguimiento de objetivos y tareas. Puedes crear objetivos y tareas, asignarles fechas de vencimiento y categorías, y hacer un seguimiento de tu progreso. Ziel te ayuda a mantenerte enfocado y a alcanzar tus metas.";
+    "Ziel es una aplicación de seguimiento de objetivos y tareas. Puedes crear objetivos y tareas, asignarles fechas y categorías, y hacer un seguimiento de tu progreso. Ziel te ayuda a mantenerte enfocado y a alcanzar tus metas.";
   const instructions =
-    'Para comenzar, haz clic en el botón "Crear objetivo" en la barra de navegación para crear un nuevo objetivo. Puedes ver tus objetivos en la página de objetivos y tus tareas en la página de tareas. También puedes filtrar tus objetivos y tareas por categoría. ';
+    'Para comenzar, haz clic en el botón "Crear Objetivo" en la barra de navegación para crear un nuevo objetivo.';
 
   const calltoAction =
-    "Revisa tus objetivos y tareas por cumplir en la página Hoy.";
+    "Revisa tus objetivos y tareas por cumplir en la página 'Diario'.";
 
   return (
     <div className="flex flex-col justify-center m-4">
       <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         {title}
       </h1>
-      <p className="mt-4 text-lg text-left text-gray-500 dark:text-gray-400">
+      <p className="mt-4 text-lg text-left text-gray-400 dark:text-gray-300 lg:mx-auto lg:w-3/4 2xl:w-2/3">
         {description}
       </p>
-      <p className="mt-4 text-md text-left text-gray-500 dark:text-gray-400">
+      <p className="mt-8 text-sm text-left text-gray-400 dark:text-gray-300/90 lg:mx-auto lg:w-3/4 2xl:w-2/3">
         {instructions}
       </p>
 
-      <div className="flex flex-col justify-center items-center mt-8 w-full mx-auto">
-        <div className="flex justify-center w-full mx-auto">
-          <Link
-            href="/objectives/new"
-            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      <div className="flex flex-col justify-center items-center mt-8 max-w-[500px] mx-auto">
+        <div className="flex flex-col xs:flex-row justify-center xs:w-auto w-full mx-auto">
+          <Button
+            variant="lineal"
+            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium"
           >
-            Crear nuevo objetivo
-          </Link>
-          <Link
-            href="/tasks/new"
-            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            <Link
+              href="/objectives/new"
+              className="text-red-500 xs:text-blue-500"
+            >
+              Crear Objetivo
+            </Link>
+          </Button>
+          <Button
+            variant="lineal"
+            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium"
           >
-            Crear nueva tarea
-          </Link>
+            <Link href="/tasks/new">Crear Tarea</Link>
+          </Button>
         </div>
-        <Link
-          href="/today"
-          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm py-2.5 px-5 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-1/2 text-center"
-        >
-          Hoy
-        </Link>
+        <div className="flex flex-col xs:flex-row justify-center xs:w-2/3 w-full mx-auto">
+          <Button
+            variant="success"
+            className="font-medium rounded-lg text-sm py-2.5 px-5 mt-2 text-center w-full"
+          >
+            <Link href="/daily">Diario</Link>
+          </Button>
+        </div>
         <p className="mt-4 text-md text-left text-gray-700 dark:text-gray-200">
           {calltoAction}
         </p>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -18,6 +18,10 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        lineal:
+          "text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700",
+        success:
+          "text-gray-100 bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:outline-none dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -31,26 +35,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
